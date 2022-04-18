abafca76e9725cf60b989b922be3c5ed406cb5fb
guitrentini96 <guitrentini96@gmail.com>
2022-04-18 21:21:41 +0200

    models created and registered at the admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'number')
+
+
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'question')
+
+
+admin.site.register(Show)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Option, OptionAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Show(models.Model):
+    title = models.CharField(max_length=100)
+    humor_score = models.SmallIntegerField()
+    thriller_score = models.SmallIntegerField()
+    romance_score = models.SmallIntegerField()
+    drama_score = models.SmallIntegerField()
+    horror_score = models.SmallIntegerField()
+    family_score = models.SmallIntegerField()
+    img_link = models.CharField(max_length=500)
+    imdb_score = models.FloatField()
+    imdb_link = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    number = models.SmallIntegerField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Option(models.Model):
+    title = models.CharField(max_length=100)
+    humor_factor = models.SmallIntegerField()
+    thriller_factor = models.SmallIntegerField()
+    romance_factor = models.SmallIntegerField()
+    drama_factor = models.SmallIntegerField()
+    horror_factor = models.SmallIntegerField()
+    family_factor = models.SmallIntegerField()
+    question = models.ForeignKey(
+        Question, related_name='options', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
