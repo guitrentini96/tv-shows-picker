c004fb339248f0f570fac2d4d6b5b6a29b7f7e06
guitrentini96 <guitrentini96@gmail.com>
2022-04-20 00:51:42 +0200

    frontend completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,10 +18,11 @@ function App() {
     testStarted: false,
     testEnded: false,
     currentScores: [5, 5, 5, 5, 5, 5],
+    shows: [],
   });
 
   React.useEffect(() => {
-    const fetchQuestions = async () => {
+    const fetchQuestionsAndOptions = async () => {
       const resQuestions = await fetch('http://localhost:8000/api/questions/');
       const returnedQuestionsList = await resQuestions.json();
       console.log('these are the questions:');
@@ -30,14 +31,19 @@ function App() {
       const returnedOptionsList = await resOptions.json();
       console.log('these are the options:');
       console.log(returnedOptionsList);
+      const resShows = await fetch('http://localhost:8000/api/shows/');
+      const returnedShowsList = await resShows.json();
+      console.log('these are the shows:');
+      console.log(returnedShowsList);
 
       setState({
         ...state,
         questions: returnedQuestionsList,
         options: returnedOptionsList,
+        shows: returnedShowsList,
       });
     };
-    fetchQuestions().catch(console.error);
+    fetchQuestionsAndOptions().catch(console.error);
   }, []);
 
   const renderQuestion = (current_question) => {
@@ -47,11 +53,11 @@ function App() {
     const filteredQuestions = state.questions.filter((question) => {
       return parseInt(question.number) === current_question;
     });
-    if (filteredQuestions.length > 0) {
-      return filteredQuestions[0].title;
-    } else {
-      setState({ ...state, testEnded: true });
-    }
+    // if (filteredQuestions.length > 0) {
+    return filteredQuestions[0].title;
+    // } else {
+    //   setState({ ...state, testEnded: true });
+    // }
   };
 
   const nextQuestion = (event) => {
@@ -84,7 +90,7 @@ function App() {
     state.questions.length === state.currentQuestion
       ? setState({
           ...state,
-          currentQuestion: 0,
+          testEnded: true,
           currentScores: newCurrentScores,
         })
       : setState({
@@ -95,8 +101,11 @@ function App() {
   };
 
   const renderOptions = (question_num) => {
+    const currentQuestionID = state.questions.filter(
+      (question) => question.number === question_num
+    )[0].id;
     const filteredOptions = state.options.filter(
-      (option) => option.question === question_num
+      (option) => option.question === currentQuestionID
     );
     const mappedOptions = filteredOptions.map((option) => (
       <Button value={option.id} key={option.id} onClick={nextQuestion}>
@@ -106,10 +115,104 @@ function App() {
     return mappedOptions;
   };
 
+  const renderResults = () => {
+    // adds a difference key to all shows in a new array
+    const newShowsList = state.shows.map(function(show) {
+      const difference =
+        Math.abs(show.humor_score - state.currentScores[0]) +
+        Math.abs(show.thriller_score - state.currentScores[1]) +
+        Math.abs(show.romance_score - state.currentScores[2]) +
+        Math.abs(show.drama_score - state.currentScores[3]) +
+        Math.abs(show.horror_score - state.currentScores[4]) +
+        Math.abs(show.family_score - state.currentScores[5]);
+      show.difference = difference;
+      return show;
+    });
+
+    function compare_differences(a, b) {
+      if (a.difference < b.difference) {
+        return -1;
+      }
+      if (a.difference > b.difference) {
+        return 1;
+      }
+      return 0;
+    }
+    // sorts then by the least difference from the overall scores
+    newShowsList.sort(compare_differences);
+
+    console.log('after sorted, the new list looks like this:');
+    console.log(newShowsList);
+
+    const top3 = newShowsList.slice(0, 3);
+    console.log('the top 3 are:');
+    console.log(top3);
+
+    const top3banner = top3.map(function(show, i) {
+      return (
+        <Paper
+          key={show.id}
+          elevation={10}
+          sx={{ backgroundColor: 'rgb(0, 30, 60)', padding: 2, width: '300px' }}
+        >
+          <Stack alignItems="center" spacing={1}>
+            <Typography variant="h4" color="white">
+              {show.title}
+            </Typography>
+
+            <img
+              src={show.img_link}
+              alt="shows cover"
+              style={{ width: '80%' }}
+            />
+            <Typography variant="subtitle1">
+              <Link
+                href={show.imdb_link}
+                underline="hover"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                imdb score: {show.imdb_score}
+              </Link>
+            </Typography>
+          </Stack>
+        </Paper>
+      );
+    });
+
+    return top3banner;
+  };
+
+  const renderRetryButton = () => {
+    const retryButton = (
+      <Button
+        variant="outlined"
+        onClick={() =>
+          setState({
+            ...state,
+            currentQuestion: 1,
+            testStarted: false,
+            testEnded: false,
+            currentScores: [5, 5, 5, 5, 5, 5],
+          })
+        }
+      >
+        Try again
+      </Button>
+    );
+    return retryButton;
+  };
+
   return (
     <>
       <Container>
-        <Stack mt={2} alignItems="center">
+        <Stack
+          mt={2}
+          mb={8}
+          alignItems="center"
+          spacing={5}
+          justifyContent="center"
+        >
           {/* if the test hasn't started, renders a welcome message, otherwise renders the question or the results */}
           <Typography variant="h3" component="h1">
             {state.testStarted
@@ -119,11 +222,22 @@ function App() {
               : "Welcome to this TV-show picker, let's start the test !"}
           </Typography>
 
-          {/* renders a start button if the test has not started, otherwise it renders the current options */}
+          {/* renders a start button if the test has not started, otherwise it renders the current options or the results */}
           {state.testStarted ? (
-            <ButtonGroup orientation="vertical">
-              {renderOptions(state.currentQuestion)}
-            </ButtonGroup>
+            state.testEnded ? (
+              <Stack
+                spacing={2}
+                sx={{
+                  justifyContent: 'center',
+                }}
+              >
+                {renderResults()}
+              </Stack>
+            ) : (
+              <ButtonGroup orientation="vertical" size="large">
+                {renderOptions(state.currentQuestion)}
+              </ButtonGroup>
+            )
           ) : (
             <Button
               variant="contained"
@@ -132,6 +246,32 @@ function App() {
               start
             </Button>
           )}
+          {state.testEnded ? renderRetryButton() : ''}
+        </Stack>
+        <Stack
+          padding={1}
+          position="fixed"
+          left="0"
+          bottom="0"
+          width="100vw"
+          bgcolor="rgb(0, 30, 60)"
+          direction="row"
+          justifyContent="center"
+        >
+          <Typography color="white">
+            {' '}
+            built by{' '}
+            <Link
+              href="https://github.com/guitrentini96"
+              underline="hover"
+              target="_blank"
+              rel="noopener"
+              sx={{ color: 'white' }}
+            >
+              Guilherme Campara
+            </Link>{' '}
+            :)
+          </Typography>
         </Stack>
       </Container>
     </>
